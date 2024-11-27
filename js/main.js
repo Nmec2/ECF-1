1b0d2f98df077d8f04c83ea9d085f9628e7dca69
Nmec2 <contact.nmec@gmail.com>
2024-11-27 16:03:27 +0100

    add toggle button to show or hide filter section

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 let newsletterId = document.getElementById('newsletter');
 
+let filterId = document.getElementById('filter');
+
 function closeNews() {
     newsletterId.style.display = "none";
+}
+
+function filterToggle() {
+    if (filterId.style.display == "block") {
+        filterId.style.display = "none";
+    } else {
+        filterId.style.display = "block";
+    }
 }
